f35ba46da0b03dbcda5da20d7beefbf69a6f6e33
Ubaidillah Hakim Fadly <ubaidillah.hakim@sinbad.co.id>
2023-04-25 15:26:44 +0700

    svr V1

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Hello World V1!")
+		fmt.Fprintln(w, "Server V1!")
 	})
 
 	http.ListenAndServe(":8081", nil)
